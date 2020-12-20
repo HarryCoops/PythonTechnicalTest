64c7ea4527bc523a724f001bfce59b6afd1a1e71
Harry Cooper <harry.coop99@gmail.com>
2020-12-20 00:11:55 +0000

    Add Bond model
    Add a Bond model with all required fields and validators for each field.
    Add a set containing all valid currency codes to utils.py for validation of
    of the currency_code field. The Bond model has a unique_together contraint
    on the user and isin fields, as the isin uniquely identifies a bond for each
    user. Also add an index on the user and isin column as the lookup on user and
    isin from the GET bond view will happen often.

--- origin/bonds/models.py
+++ origin/bonds/models.py
@@ -1,1 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User  
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+
+from bonds.utils import CURRENCY_CODES
+
+def validate_positive(value):
+    if value <= 0:
+        raise ValidationError(f"{value} is not a positive integer")
+
+def validate_currency_code(value):
+    if value not in CURRENCY_CODES:
+        raise ValidationError(f"{value} currency code does not exist")
+
+def validate_isin_length(value):
+    if len(value) != 12:
+        raise ValidationError(f"{value} has invalid length for ISIN")
+
+def validate_lei_length(value):
+    if len(value) != 20:
+        raise ValidationError(f"{value} has invalid length for lei")
+
+
+class Bond(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    isin = models.CharField(max_length=12, validators=[
+        RegexValidator(
+            regex=r"^[A-Z0-9]*$",
+            message="ISIN must be an uppercase alphanumeric string",
+            code="invalid_isin"
+        ),
+        validate_isin_length
+    ])
+    size = models.IntegerField(validators=[validate_positive])
+    currency = models.CharField(max_length=3, validators=[validate_currency_code])
+    maturity = models.DateField()
+    lei = models.CharField(max_length=20, validators=[
+        RegexValidator(
+            regex=r"^[A-Z0-9]*$",
+            message="LEI must be an uppercase alphanumeric string",
+            code="invalid_lei"
+        ),
+        validate_lei_length
+    ])
+    legal_name = models.CharField(max_length=100)
+    created = models.DateField(auto_now=True)
+
+    class Meta:
+        unique_together = ("user", "isin")
+        indexes = [
+            models.Index(fields=["user", "isin"])
+        ]
+    
